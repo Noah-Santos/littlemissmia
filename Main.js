155ea5f09b345fd27a3a3bb573956c4294a99c76
Noah-Santos <nsanto591@west-mec.org>
2022-10-18 20:21:05 -0700

    Half files
    Half of the files copied over.

---
+++ Main.js
@@ -0,0 +1,10 @@
+// makes the navbar work while in a smaller screen by making it go away and come back due to the 3 lines
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
